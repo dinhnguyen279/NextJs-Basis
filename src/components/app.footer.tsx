af341a60508ee41ac17a406a4609b6a49b518411
Nguyễn <tdinhnguyen279@gmail.com>
2023-08-10 16:45:34 +0700

    style: :memo: add react bootstrap

---
+++ src/components/app.footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="bg-gray-300 py-2 text-center">Next.JS v13 & Daniel</div>
+    </footer>
+  );
+};
+
+export default Footer;
